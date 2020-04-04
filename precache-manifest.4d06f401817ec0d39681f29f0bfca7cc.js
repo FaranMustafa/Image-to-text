80d03e3dcf78cd764bb003a716a7142fac720646
FaranMustafa <fmustafa044@gmail.com>
2020-04-05 01:12:43 +0500

    Updates

---
+++ precache-manifest.4d06f401817ec0d39681f29f0bfca7cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a883156a120cc011bd27985779f0ee",
+    "url": "/Image-to-text/index.html"
+  },
+  {
+    "revision": "2356535379e650179c3a",
+    "url": "/Image-to-text/static/css/main.580a2582.chunk.css"
+  },
+  {
+    "revision": "2cdd8fba2d7525b9854f",
+    "url": "/Image-to-text/static/js/2.ce1a6af9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Image-to-text/static/js/2.ce1a6af9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2356535379e650179c3a",
+    "url": "/Image-to-text/static/js/main.e0d42997.chunk.js"
+  },
+  {
+    "revision": "8beeec9b03714f9665cc",
+    "url": "/Image-to-text/static/js/runtime-main.85ae5723.js"
+  }
+]);
